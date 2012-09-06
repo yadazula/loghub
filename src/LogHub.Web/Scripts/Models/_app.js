88a6b27c528fc9e7c244263fa2b937166dc67335
Gokhan Demir <yadazula@gmail.com>
2012-09-06 17:50:47 +0300

    knockout migration

---
+++ src/LogHub.Web/Scripts/Models/_app.js
@@ -0,0 +1,46 @@
+﻿//var loghub = loghub || {};
+//$(function () {
+//    'use strict';
+
+//    var AppRouter = Backbone.Router.extend({
+//        routes: {
+//            '': 'showRecentLogs',
+//            'dashboard': 'showRecentLogs',
+//            'search': 'showSearches',
+//            'search?p=:page': 'showSearches'
+//        },
+        
+//        showSearches: function (page) {
+//            var searchLogList = new window.loghub.SearchLogList(null, { page: page });
+//            var searchLogListView = VM.createView("searchLogListView", function () {
+//                return new window.loghub.SearchLogListView({ collection: searchLogList });
+//            });
+            
+//            $('#main').html(searchLogListView.render().el);
+//            searchLogList.fetch();
+//            this.highlightItem("#navSearch");
+//        },
+
+//        showRecentLogs: function () {
+//            var recentLogList = new window.loghub.RecentLogList;
+//            var recentLogListView = VM.createView("recentLogListView", function () {
+//                return new window.loghub.RecentLogListView({ collection: recentLogList });
+//            });
+            
+//            $('#main').html(recentLogListView.render().el);
+//            recentLogListView.stream({ interval: 5000 });
+//            this.highlightItem("#navDashboard");
+//        },
+        
+//        highlightItem: function (el) {
+//            $('#navList > li').removeClass('active');
+//            $('#navList > li > a > i').removeClass('icon-white');
+//            $(el).parent().addClass('active');
+//            $(el).children().addClass('icon-white');
+//        }
+
+//    });
+
+//    loghub.Router = new AppRouter();
+//    Backbone.history.start();
+//});
--- src/LogHub.Web/Scripts/app.js
+++ src/LogHub.Web/Scripts/app.js
@@ -1,46 +1,60 @@
 ﻿var loghub = loghub || {};
-$(function () {
-    'use strict';
+loghub.viewmodels = loghub.viewmodels || {};
 
-    var AppRouter = Backbone.Router.extend({
-        routes: {
-            '': 'showRecentLogs',
-            'dashboard': 'showRecentLogs',
-            'search': 'showSearches',
-            'search?p=:page': 'showSearches'
-        },
-        
-        showSearches: function (page) {
-            var searchLogList = new window.loghub.SearchLogList(null, { page: page });
-            var searchLogListView = VM.createView("searchLogListView", function () {
-                return new window.loghub.SearchLogListView({ collection: searchLogList });
-            });
-            
-            $('#main').html(searchLogListView.render().el);
-            searchLogList.fetch();
-            this.highlightItem("#navSearch");
-        },
+loghub.viewmodels.Page = function (url, icon, name) {
+    this.url = url;
+    this.icon = icon;
+    this.name = name;
+    this.isActive = ko.observable(false);
+};
 
-        showRecentLogs: function () {
-            var recentLogList = new window.loghub.RecentLogList;
-            var recentLogListView = VM.createView("recentLogListView", function () {
-                return new window.loghub.RecentLogListView({ collection: recentLogList });
-            });
-            
-            $('#main').html(recentLogListView.render().el);
-            recentLogListView.stream({ interval: 5000 });
-            this.highlightItem("#navDashboard");
-        },
-        
-        highlightItem: function (el) {
-            $('#navList > li').removeClass('active');
-            $('#navList > li > a > i').removeClass('icon-white');
-            $(el).parent().addClass('active');
-            $(el).children().addClass('icon-white');
+loghub.App = function () {
+    var self = this;
+    var dashboard = new loghub.viewmodels.Page('#dashboard', 'icon-home', 'Dashboard');
+    var searches = new loghub.viewmodels.Page('#searches', 'icon-search', 'Searches');
+    var settings = new loghub.viewmodels.Page('#settings', 'icon-wrench', 'Settings');
+    self.pages = [dashboard, searches, settings];
+
+    self.setCurrentPage = function (page, templateName, data) {
+        if (self.currentPage) {
+            self.currentPage.isActive(false);
         }
 
-    });
+        if (self.currentPageEl) {
+            ko.removeNode(self.currentPageEl);
+        }
 
-    loghub.Router = new AppRouter();
-    Backbone.history.start();
-});
+        if (self.currentPageObservable) {
+            self.currentPageObservable.dispose();
+        }
+
+        self.currentPage = page;
+        self.currentPage.isActive(true);
+
+        var el = $('#pageContainer')[0];
+        self.currentPageObservable = ko.renderTemplate(templateName, data, {}, el);
+        if (el.children.length === 1) {
+            self.currentPageEl = el.children[0];
+        }
+    };
+
+    Sammy(function () {
+        this.get('#dashboard', function () {
+            var log = { Date: 'Host', Host: 'Host', Source: 'Source', Logger: 'Logger', Level: 'Level', Message: 'Message' };
+            var logs = { logItems: [log] }; //TODO : dispose logs
+            self.setCurrentPage(dashboard, 'recent-log-list-template', logs);
+        });
+
+        this.get('#searches', function () {
+            self.setCurrentPage(searches,'', {});
+        });
+
+        this.get('#settings', function () {
+            self.setCurrentPage(settings, '', {});
+        });
+
+        this.get('', function () { this.app.runRoute('get', '#dashboard'); });
+    }).run();
+};
+
+ko.applyBindings(new loghub.App());
