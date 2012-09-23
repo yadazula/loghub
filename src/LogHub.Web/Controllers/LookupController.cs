5b925567d5dfc2d63c79ca8bef928f51f24bd5f5
Gokhan Demir <yadazula@gmail.com>
2012-09-23 20:44:14 +0300

    lookup & autocomplete

---
+++ src/LogHub.Web/Controllers/LookupController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using LogHub.Web.Infrastructure.Common;
+using LogHub.Web.Models;
+using LogHub.Web.ViewModels;
+using Raven.Client;
+using Raven.Client.Linq;
+
+namespace LogHub.Web.Controllers
+{
+  public class LookupController : AbstractApiController
+  {
+    public LookupController(IDocumentSession documentSession)
+      : base(documentSession)
+    {
+    }
+
+    public IEnumerable<string> Get([FromUri]LookupFilter lookupFilter)
+    {
+      if (lookupFilter.Type == "source")
+      {
+        return GetSources(lookupFilter.Query);
+      }
+
+      if (lookupFilter.Type == "host")
+      {
+        return GetHosts(lookupFilter.Query);
+      }
+
+      return Enumerable.Empty<string>();
+    }
+
+    private IEnumerable<string> GetSources(string query)
+    {
+      if (query.IsNullOrWhiteSpace())
+      {
+        return DocumentSession.Query<LogMessage>()
+                              .Select(x => x.Source)
+                              .Distinct()
+                              .ToList()
+                              .OrderBy(x => x);
+      }
+
+      return DocumentSession.Query<LogMessage>()
+                            .Where(x => x.Source.StartsWith(query))
+                            .Select(x => x.Source)
+                            .Distinct()
+                            .ToList()
+                            .OrderBy(x => x);
+    }
+
+    private IEnumerable<string> GetHosts(string query)
+    {
+      if (query.IsNullOrWhiteSpace())
+      {
+        return DocumentSession.Query<LogMessage>()
+                              .Select(x => x.Host)
+                              .Distinct()
+                              .ToList()
+                              .OrderBy(x => x);
+      }
+
+      return DocumentSession.Query<LogMessage>()
+                            .Where(x => x.Host.StartsWith(query))
+                            .Select(x => x.Host)
+                            .Distinct()
+                            .ToList()
+                            .OrderBy(x => x);
+    }
+  }
+}
---
+++ src/LogHub.Web/ViewModels/LookupFilter.cs
@@ -0,0 +1,8 @@
+namespace LogHub.Web.ViewModels
+{
+  public class LookupFilter
+  {
+    public string Type { get; set; }
+    public string Query { get; set; }
+  }
+}
