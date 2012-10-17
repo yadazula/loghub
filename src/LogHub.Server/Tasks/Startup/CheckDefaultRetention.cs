4c5b6fcadba056552679b3729e78dd9b035ebe25
Gokhan Demir <yadazula@gmail.com>
2012-10-17 18:13:18 +0300

    added default retention

---
+++ src/LogHub.Server/Tasks/Startup/CheckDefaultRetention.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using LogHub.Core.Models;
+using Raven.Client;
+
+namespace LogHub.Server.Tasks.Startup
+{
+	public class CheckDefaultRetention : IStartupTask
+	{
+		private readonly IDocumentStore documentStore;
+
+		public CheckDefaultRetention(IDocumentStore documentStore)
+		{
+			this.documentStore = documentStore;
+		}
+
+		public void Execute()
+		{
+			using (var documentSession = documentStore.OpenSession())
+			{
+				var retention = documentSession.Query<Retention>().FirstOrDefault();
+				
+				if (retention != null)
+					return;
+
+				retention = new Retention
+				{
+					Days = 60, 
+					CreatedBy = "System", 
+					CreatedAt = DateTimeOffset.Now
+				};
+
+				documentSession.Store(retention);
+				documentSession.SaveChanges();
+			}
+		}
+	}
+}
