ebe4d8a832cd1d442cf1be2b3b6d1ed16d11b949
Gokhan Demir <yadazula@gmail.com>
2012-10-18 16:10:03 +0300

    logalert automapper fix

--- src/LogHub.Web/Infrastructure/AutoMapper/AutoMapperConfiguration.cs
+++ src/LogHub.Web/Infrastructure/AutoMapper/AutoMapperConfiguration.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using AutoMapper;
 using LogHub.Core.Models;
 using LogHub.Web.ViewModels;
+using LogHub.Core.Extensions;
 
 namespace LogHub.Web.Infrastructure.AutoMapper
 {
@@ -19,12 +20,24 @@ namespace LogHub.Web.Infrastructure.AutoMapper
 
 			Mapper.CreateMap<LogAlert, LogAlertView>()
 				.ForMember(x => x.Minutes, o => o.MapFrom(m => m.Minutes.TotalMinutes))
-				.ForMember(x => x.EmailTo, o => o.MapFrom(m => string.Join(",", m.EmailToList)));
+				.ForMember(x => x.EmailTo, o => o.ResolveUsing(m =>
+					{
+						if (m.EmailToList.IsNull())
+							return string.Empty;
+
+						return string.Join(",", m.EmailToList);
+					}
+					));
 
 			Mapper.CreateMap<LogAlertView, LogAlert>()
 				.ForMember(x => x.Minutes, o => o.MapFrom(m => TimeSpan.FromMinutes(m.Minutes)))
 				.ForMember(x => x.EmailToList, o => o.ResolveUsing(m =>
 					{
+						if (m.EmailTo.IsNullOrWhiteSpace())
+						{
+							return new List<string>();
+						}
+
 						var emails = m.EmailTo.Split(',');
 						return emails.Select(email => email.Trim()).ToList();
 					}));
