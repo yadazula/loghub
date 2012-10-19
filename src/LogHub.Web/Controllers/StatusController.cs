64fcbc616e59fe30c229c3a22efa0a30f643de6d
Gokhan Demir <yadazula@gmail.com>
2012-10-20 01:56:50 +0300

    finished webui status controller

--- src/LogHub.Web/Controllers/StatusController.cs
+++ src/LogHub.Web/Controllers/StatusController.cs
@@ -1,6 +1,10 @@
-﻿using System.Net;
-using System.Net.Sockets;
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
 using LogHub.Core.Models;
+using NLog;
+using Newtonsoft.Json;
 using Raven.Client;
 using Raven.Client.Connection;
 using Raven.Client.Document;
@@ -9,62 +13,69 @@ namespace LogHub.Web.Controllers
 {
 	public class StatusController : AbstractApiController
 	{
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
 		public StatusController(IDocumentSession documentSession)
 			: base(documentSession)
 		{
 		}
 
-		public SystemStatus Get()
+		public async Task<SystemStatus> Get()
 		{
 			var systemStatus = new SystemStatus();
-
-			GetRavenStats(systemStatus);
+			systemStatus.Database = await GetDatabaseInfo();
+			systemStatus.Server = await GetServerInfo();
 
 			return systemStatus;
 		}
 
-		private void GetRavenStats(SystemStatus systemStatus)
+		private async Task<SystemStatus.DatabaseInfo> GetDatabaseInfo()
 		{
 			var documentStore = (DocumentStore)DocumentSession.Advanced.DocumentStore;
 			try
 			{
 				var versionRequestParams = new CreateHttpJsonRequestParams(null, documentStore.Url + "/build/version", "GET", documentStore.Credentials, documentStore.Conventions);
-				var version = documentStore.JsonRequestFactory.CreateHttpJsonRequest(versionRequestParams).ReadResponseJson();
+				var version = await documentStore.JsonRequestFactory.CreateHttpJsonRequest(versionRequestParams).ReadResponseJsonAsync();
 
 				var dbSizeRequestParams = new CreateHttpJsonRequestParams(null, documentStore.Url + "/database/size", "GET", documentStore.Credentials, documentStore.Conventions);
-				var dbSize = documentStore.JsonRequestFactory.CreateHttpJsonRequest(dbSizeRequestParams).ReadResponseJson();
+				var dbSize = await documentStore.JsonRequestFactory.CreateHttpJsonRequest(dbSizeRequestParams).ReadResponseJsonAsync();
 
-				var databaseStatistics = documentStore.DatabaseCommands.GetStatistics();
+				var databaseStatistics = await documentStore.AsyncDatabaseCommands.GetStatisticsAsync();
 
-				systemStatus.Database.Version = version.Value<string>("ProductVersion");
-				systemStatus.Database.Size = dbSize.Value<string>("DatabaseSizeHumane");
-				systemStatus.Database.CountOfDocuments = databaseStatistics.CountOfDocuments;
-				systemStatus.Database.CountOfIndexes = databaseStatistics.CountOfIndexes;
-				systemStatus.Database.Status = SystemStatus.ConnectionState.Online;
+				var databaseInfo = new SystemStatus.DatabaseInfo();
+				databaseInfo.Version = version.Value<string>("ProductVersion");
+				databaseInfo.Size = dbSize.Value<string>("DatabaseSizeHumane");
+				databaseInfo.CountOfDocuments = databaseStatistics.CountOfDocuments;
+				databaseInfo.CountOfIndexes = databaseStatistics.CountOfIndexes;
+				databaseInfo.Status = SystemStatus.ConnectionState.Online;
+				return databaseInfo;
 			}
-			catch (WebException e)
+			catch (Exception exception)
 			{
-				var socketException = e.InnerException as SocketException;
-				if (socketException == null)
-					throw;
+				Logger.Error(exception);
+				var databaseInfo = new SystemStatus.DatabaseInfo { Status = SystemStatus.ConnectionState.Offline };
+				return databaseInfo;
+			}
+		}
 
-				switch (socketException.SocketErrorCode)
+		private async Task<SystemStatus.ServerInfo> GetServerInfo()
+		{
+			try
+			{
+				using (var httpClient = new HttpClient())
 				{
-					case SocketError.AddressNotAvailable:
-					case SocketError.NetworkDown:
-					case SocketError.NetworkUnreachable:
-					case SocketError.ConnectionAborted:
-					case SocketError.ConnectionReset:
-					case SocketError.TimedOut:
-					case SocketError.ConnectionRefused:
-					case SocketError.HostDown:
-					case SocketError.HostUnreachable:
-					case SocketError.HostNotFound:
-						systemStatus.Database.Status = SystemStatus.ConnectionState.Offline;
-						break;
-					default:
-						throw;
+					var statusUrl = string.Format("{0}/api/status", ConfigurationManager.AppSettings["ServerAddress"]);
+					var responseString = await httpClient.GetStringAsync(statusUrl);
+					var serverInfo = JsonConvert.DeserializeObject<SystemStatus.ServerInfo>(responseString);
+					serverInfo.Status = SystemStatus.ConnectionState.Online;
+					return serverInfo;
 				}
+			}
+			catch (Exception exception)
+			{
+				Logger.Error(exception);
+				var serverInfo = new SystemStatus.ServerInfo { Status = SystemStatus.ConnectionState.Offline };
+				return serverInfo;
 			}
 		}
 	}
